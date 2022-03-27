41999e5a6558609f62b969b5876c8007f09ca119
Aruna3253 <84565442+Aruna3253@users.noreply.github.com>
2022-03-27 20:32:29 +0545

    Add files via upload

---
+++ goal/include/left-nav.php
@@ -0,0 +1,8 @@
+<div class="col-4">
+            <ul class="list-group">
+            <li class="list-group-item"><a href="goal.php">Add goal</a></li>
+            <li class="list-group-item"><a href="add-goal.php">Add Post</a></li>
+            <li class="list-group-item"><a href="goal.php">Add Post</a></li>
+
+</ul>
+</div>
